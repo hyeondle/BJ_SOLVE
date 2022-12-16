417141b9cfcc09bf3119a3096e8b35f7bf51189f
Linsio <103421762+hyeondle@users.noreply.github.com>
2022-12-16 17:14:24 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,a,b;
+    scanf("%d", &i);
+    for (int j = 0;j<i;j++)
+    {
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n",j+1,a,b,a+b);
+    }
+}
