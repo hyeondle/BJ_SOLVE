6519b268cdaafe8d530303dce7b31101c7802b4e
Linsio <103421762+hyeondle@users.noreply.github.com>
2024-07-13 20:46:24 +0900

    [Bronze I] Title: 이항 계수 1, Time: 48 ms, Memory: 33240 KB -BaekjoonHub

---
+++ Python/백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,10 @@
+import math
+import sys
+
+input = sys.stdin.read
+
+def binomial_coefficient(n, k):
+    return math.factorial(n) // (math.factorial(k) * math.factorial(n - k))
+
+n, k = map(int, input().split())
+print(binomial_coefficient(n, k))
