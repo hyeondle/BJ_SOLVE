7361300077ee1a34e003d2ae025cd2d6d198bfdf
Linsio <103421762+hyeondle@users.noreply.github.com>
2024-07-20 03:21:01 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- Python/백준/Bronze/15829. Hashing/Hashing.py
+++ Python/백준/Bronze/15829. Hashing/Hashing.py
@@ -1,7 +1,21 @@
-L = int(input())
-S = input()
-res = 0
-d = "0abcdefghijklmnopqrstuvwxyz"
-for _ in range(L) :
-    res += (d.find(S[_]) * (31 ** (_))) % 1234567891
-print(res)
+def compute_hash(s):
+    r = 31
+    M = 1234567891
+    hash_value = 0
+
+    for i, char in enumerate(s):
+        a_i = ord(char) - ord('a') + 1
+        hash_value += a_i * (r ** i)
+        hash_value %= M
+    
+    return hash_value
+
+
+n = int(input().strip())
+s = input().strip()
+
+
+result = compute_hash(s)
+
+
+print(result)
