ac152bd616a84b02b585736245bb2bf407179f9e
Linsio <103421762+hyeondle@users.noreply.github.com>
2024-07-24 21:50:50 +0900

    Update IOIOI.py

--- Python/백준/Silver/5525. IOIOI/IOIOI.py
+++ Python/백준/Silver/5525. IOIOI/IOIOI.py
@@ -18,3 +18,28 @@ for i in range(m - l + 1) :
         if miss == 0 :
             count += 1
 print(count)
+
+###
+#n = int(input())
+#m = int(input())
+#s = input()
+
+#pattern_length = 2 * n + 1
+#count = 0
+#i = 0
+#result = 0
+
+#while i < (m - 1):
+    #if s[i:i+3] == 'IOI':
+        #count += 1
+        #if count == n:
+            #count -= 1
+            #result += 1
+        #i += 2
+    #else:
+        #count = 0
+        #i += 1
+
+#print(result)
+#100점짜리
+###
